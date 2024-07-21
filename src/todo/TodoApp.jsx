3d04a6707e9f0b0f24c86a31b99716025d71fbec
me-coderaider <kashyappankajnsp@gmail.com>
2024-07-21 10:30:30 +0530

    added Header, Footer & Logout Component

--- src/todo/TodoApp.jsx
+++ src/todo/TodoApp.jsx
@@ -12,6 +12,7 @@ import "./TodoApp.css";
 export default function TodoApp() {
 	return (
 		<div className="TodoApp">
+			<HeaderComponent />
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<LoginComponent />}></Route>
@@ -21,10 +22,12 @@ export default function TodoApp() {
 						element={<WelcomeComponent />}
 					></Route>
 					<Route path="/todos" element={<ListTodosComponent />}></Route>
+					<Route path="/logout" element={<LogoutComponent />}></Route>
 
 					<Route path="*" element={<ErrorComponent />}></Route>
 				</Routes>
 			</BrowserRouter>
+			<FooterComponent />
 		</div>
 	);
 }
@@ -174,6 +177,32 @@ function ListTodosComponent() {
 					</tbody>
 				</table>
 			</div>
+		</div>
+	);
+}
+
+function HeaderComponent() {
+	return (
+		<div className="header">
+			Header <hr />
+		</div>
+	);
+}
+
+function FooterComponent() {
+	return (
+		<div className="footer">
+			<hr />
+			Footer
+		</div>
+	);
+}
+
+function LogoutComponent() {
+	return (
+		<div className="LogoutComponent">
+			<h1>You are logged out!</h1>
+			<div>Thank you for using our App. Come back soon!</div>
 		</div>
 	);
 }
