70d541c3e7ec511d0217741807e5e44a2edc2380
me-coderaider <kashyappankajnsp@gmail.com>
2024-07-28 15:34:52 +0530

    adding SpringBoot DeleteTodoApi with React Frontend

--- src/todo/ListTodosComponent.jsx
+++ src/todo/ListTodosComponent.jsx
@@ -1,3 +1,10 @@
+import { useEffect, useState } from "react";
+import {
+	deleteTodoApi,
+	retrieveAllTodosForUsernameApi,
+} from "./api/TodoApiService";
+import { getValue } from "@testing-library/user-event/dist/utils";
+
 function ListTodosComponent() {
 	const today = new Date();
 
@@ -7,47 +14,80 @@ function ListTodosComponent() {
 		today.getDate()
 	);
 
-	const todos = [
-		{
-			id: 1,
-			description: "Learn SpringBoot",
-			done: false,
-			targetDate: targetDate,
-		},
-		{
-			id: 2,
-			description: "Learn Nginx",
-			done: false,
-			targetDate: targetDate,
-		},
-		{
-			id: 3,
-			description: "Remember DesignPatterns",
-			done: false,
-			targetDate: targetDate,
-		},
-	];
+	const [todos, setTodos] = useState([]);
+	const [message, setMessage] = useState(null);
+
+	useEffect(() => refreshTodos(), []);
+
+	function refreshTodos() {
+		retrieveAllTodosForUsernameApi("coderaider")
+			.then((response) => setTodos(response.data))
+			.catch((error) => console.log(error));
+	}
+
+	function deleteTodo(id) {
+		console.log("delete todo", id);
+		deleteTodoApi("coderaider", id)
+			.then(
+				() => {
+					setMessage(`Delete of todo with id= ${id} successful.`);
+					refreshTodos();
+				}
+				//1. Display message
+				//2. Update Todos List
+			)
+			.catch((error) => console.log(error));
+	}
+
+	// const todos = [
+	// 	{
+	// 		id: 1,
+	// 		description: "Learn SpringBoot",
+	// 		done: false,
+	// 		targetDate: targetDate,
+	// 	},
+	// 	{
+	// 		id: 2,
+	// 		description: "Learn Nginx",
+	// 		done: false,
+	// 		targetDate: targetDate,
+	// 	},
+	// 	{
+	// 		id: 3,
+	// 		description: "Remember DesignPatterns",
+	// 		done: false,
+	// 		targetDate: targetDate,
+	// 	},
+	// ];
 
 	return (
 		<div className="container">
 			<h1>Things you want to do!</h1>
+			{message && <div className="alert alert-warning">{message}</div>}
 			<div>
 				<table className="table">
 					<thead>
 						<tr>
-							<td>Id</td>
-							<td>Description</td>
-							<td>Is done?</td>
-							<td>Target Date</td>
+							<th>Description</th>
+							<th>Is done?</th>
+							<th>Target Date</th>
+							<th>Delete</th>
 						</tr>
 					</thead>
 					<tbody>
 						{todos.map((todo) => (
 							<tr key={todo.id}>
-								<td>{todo.id}</td>
 								<td>{todo.description}</td>
 								<td>{todo.done.toString()}</td>
-								<td>{todo.targetDate.toDateString()}</td>
+								<td>{todo.targetDate.toString()}</td>
+								<td>
+									<button
+										className="btn btn-warning"
+										onClick={() => deleteTodo(todo.id)}
+									>
+										Delete
+									</button>
+								</td>
 							</tr>
 						))}
 					</tbody>
